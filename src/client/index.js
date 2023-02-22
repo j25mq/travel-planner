c2972d766e7c3f6dcc4fdb80c90704f6630da5b1
julie <juliemiquelard@protonmail.com>
2023-02-22 09:29:33 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import './styles/main.css';
+import './styles/app.css';
+import './styles/footer.css';
+import './styles/header.css';
+import './styles/index.css';
+
+// import { validateURL } from './js/urlChecker';
+// import { handleSubmit } from './js/formHandler';
+
+// export {validateURL, handleSubmit};

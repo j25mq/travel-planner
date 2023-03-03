d16452829ab4f83c5fd32fdf46b45b3515d60814
julie <juliemiquelard@protonmail.com>
2023-03-03 08:55:50 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,13 +4,19 @@ const addTrip = document.getElementById('generate');
 const fetchInput = async() => {
     const location = document.getElementById('location').value;
     const date = document.getElementById('date').value;
+    if (!date) {
+        window.alert(`Please enter a date.`);
+        return;
+    }
+
     const flight = document.getElementById('flightInfo').value;
     const hosting = document.getElementById('hostingInfo').value;
     const notes = document.getElementById('notes').value;
 
-    const request = await fetch("http://localhost:8080");
-
     const myTrip = {};
+
+    // set ID
+    myTrip.id = Date.now();
 
     // location input
     myTrip.location = location;
@@ -26,12 +32,10 @@ const fetchInput = async() => {
         return diffInDays;
     };
 
-    daysAway = getNumberOfDays(new Date(), date);
+    const daysAway = getNumberOfDays(new Date(), date);
 
-    if ((daysAway < 0)) { // date before today's date is handled as an error
+    if (daysAway < 0) { // date before today's date is handled as an error
         window.alert(`It seems that the date entered is before todays' date. Please enter a valid date.`);
-    } else {
-        window.alert(`Please enter a date.`);
     };
 
     myTrip.date = date;
@@ -45,10 +49,6 @@ const fetchInput = async() => {
 
     // notes input
     myTrip.notes = notes;
-
-    // console.log("myTrip", myTrip);
-    localStorage.setItem('my-trip', JSON.stringify(myTrip));
-    // console.log("ls.my-trip", localStorage.getItem('my-trip'));
     
     // append the last trip to the existing list of trips
     const myTripsStr = localStorage.getItem('my-trips');
@@ -61,11 +61,17 @@ const fetchInput = async() => {
         myTrips = [];
     };
 
+    console.log("myTrip", myTrip);
+    
     myTrips.push(myTrip);
+    console.log("myTrips", myTrips);
     localStorage.setItem('my-trips', JSON.stringify(myTrips));
+
+    const newTripAdded = document.getElementById('newTripAdded');
+    newTripAdded.innerHTML = `A new trip to <b>${location}</b> planned on <b>${date}</b> has been added to <a href="my-trips.html">your trips</a>.`;
 };
 
-// button disabled until location entered
+// button 'add trip' disabled until location entered
 function success() {
     if (document.getElementById('location').value==="") { 
         document.getElementById('generate').disabled = true; 
